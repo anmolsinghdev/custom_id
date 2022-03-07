f8b807b9bd9ffabb0ddf13946a3ba9db53fe5986
Anmol Singh <anmols@chetu.com>
2022-03-07 22:54:33 +0530

    final commit

--- index.js
+++ index.js
@@ -6,24 +6,9 @@ const userModel = require("./UserSchema");
 app.use(express.json());
 
 app.post("/user/create", async (req, res, next) => {
-  const { name, email } = req.body;
-
-  const data = await userModel.create({
-    name: name,
-    email: email,
-  });
-
-  res.json({
-    success: true,
-    data: data,
-  });
-});
-app.post("/user/find", async (req, res, next) => {
   const { name, email, id } = req.body;
   const fixid = "5099803df3f4948bd2f92856";
   const data = await userModel.exists({ _id: fixid });
-
-  console.log(data);
   if (data === null) {
     const data2 = await userModel.create({
       _id: id,
